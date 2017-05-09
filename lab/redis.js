613267f42e503694881b473ded87de9d44fb4726
PeckZeg <peck.zeg@gmail.com>
2017-05-09 14:31:38 +0800

    更新文档

--- lab/redis.js
+++ lab/redis.js
@@ -1,22 +1,55 @@
 const redis = require('redis');
 const bluebird = require('bluebird');
-const createApiKey = require('../utils/api-key');
-const createSecretKey = require('../utils/secret-key');
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
+const USER_ID = 65536;
 const NAME = 'PeckZeg';
 const PASSWORD = 'peckzeg65536:?{OX}';
 
-let apiKey = createApiKey(NAME, PASSWORD);
-let secretKey = createSecretKey(apiKey);
+const apiKey = 'yO392M07ar/miLtJd5FUOqxC/WJfjhZbyZxvcOvsVyo=';
+const secretKey = 'ocNL66k2u6D6e82EXZA1+l+SLjeE+NkJwG1G051H0Ac=';
+
+const hexString = str => new Buffer(String(str)).toString('hex');
 
 const client = redis.createClient();
 
-client.setAsync('foo', 123).then(res => {
-  return client.getAsync('foo');
-}).then(value => {
-  console.log(value);
-  client.quit();
-});
+// client.setAsync(`userApiKey:${apiKey}`, USER_ID).then(res => {
+//   return client.getAsync(`user-api-key:${apiKey}`);
+// }).then(userId => {
+//   console.log(userId);
+//   client.quit();
+// });
+
+// client.hmset(`userApiKey:${apiKey}`, { apiKey, secretKey }).then(res => {
+//   console.log(res);
+// });
+
+let keyByUserId = `user-id-api-key:${hexString(USER_ID)}`;
+let keyByApiKey = `user-api-key:${hexString(apiKey)}`;
+
+// client.hmset(keyByUserId, { apiKey, secretKey }, res => {
+//   console.log(res);
+// });
+
+// client.hmsetAsync(keyByUserId, { apiKey, secretKey, userId: USER_ID }).then(res => {
+//   return client.hgetallAsync(keyByUserId);
+// }).then(values => {
+//   console.log(values);
+// });
+
+
+client.multi()
+  .set(keyByUserId, apiKey)
+  .hmset(keyByApiKey, { apiKey, secretKey, userId: USER_ID })
+  .execAsync()
+  .then(() => client.getAsync(keyByUserId))
+  .then(apiKey => {
+    console.log(keyByUserId, { apiKey });
+    return client.hgetallAsync(keyByApiKey);
+  })
+  .then(result => {
+    console.log(keyByApiKey, result);
+    client.quit();
+  });
