f68e2bbfa4f8e075a2f3d9f57108c765139c9cf3
PeckZeg <peck.zeg@gmail.com>
2017-07-20 12:12:50 +0800

    fixed

--- sync-tools/config.js
+++ sync-tools/config.js
@@ -6,4 +6,5 @@
 // };
 
 
-exports.TMP_FOLDER = process.env.NODE_RUNTIME !== void 0 ? '/data' : '/tmp';
+// exports.TMP_FOLDER = process.env.NODE_RUNTIME !== void 0 ? '/data' : '/tmp';
+exports.TMP_FOLDER = '/tmp';
