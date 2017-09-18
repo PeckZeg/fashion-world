0132995e0c22ec12bf975f03e82acf6719f882ea
PeckZeg <peck.zeg@gmail.com>
2017-09-18 11:48:12 +0800

    debug

--- config/index.js
+++ config/index.js
@@ -1,6 +1,8 @@
 const baseConfig = require('./base');
 const envConfig = require(`./${process.env.NODE_ENV || 'development'}`);
 
+console.log({NODE_ENV:process.env.NODE_ENV});
+
 let config = Object.assign({}, baseConfig, envConfig);
 
 module.exports = config;
