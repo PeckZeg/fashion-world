3547b7faa90b033f50d1e372080519886291771d
PeckZeg <peck.zeg@gmail.com>
2017-10-20 16:45:25 +0800

    update admin templates

--- api/admin/banner/GET/fetchBannerList/index.js
+++ api/admin/banner/GET/fetchBannerList/index.js
@@ -4,11 +4,13 @@ const authToken = reqlib('./utils/token/auth/account');
 const createLog = reqlib('./utils/createAccountLog');
 
 const injectProps = reqlib('./utils/model-injector/banner');
+const injectVideoProps = reqlib('./utils/model-injector/video');
 const transformQuery = require('./transformQuery');
 const validateQuery = require('./validateQuery');
 const { mergeQueryCond, setSort } = reqlib('./utils/api-model')(require('./props'));
 
 const Banner = reqlib('./models/Banner');
+const Video  = reqlib('./models/Video');
 
 const action = 'ADMIN_BANNER_GET_FETCH_BANNER_LIST';
 
@@ -41,6 +43,27 @@ module.exports = (req, res, next) => {
       Banner.find(cond).skip(skip).limit(limit).sort(sort)
         .then(banners => injectProps(banners, 'toObject'))
         .then(banners => ({ cond, banners }))
+    ))
+
+    // get videoId
+    .then(({ cond, banners }) => (
+      Video.find({
+        _id: _.chain(banners).map('value.videoId').compact().uniq().value()
+      })
+        .then(videos => injectVideoProps(null, videos, 'toObject'))
+        .then(videos => {
+          videos = _.keyBy(videos, '_id');
+
+          banners.forEach(banner => {
+            const videoId = _.get(banner, 'value.videoId');
+
+            if (videoId && videos[videoId] !== void 0) {
+              banner.video = videos[videoId];
+            }
+          });
+
+          return { cond, banners };
+        })
     ))
 
     // count banner docs
--- docs/accounts.md
+++ docs/accounts.md
@@ -1,15 +1,15 @@
 # 账号
 
-## 地图
-
 所属                        | 手机          | 账号                             | 密码
 :-------------------------- | :------------ | :------------------------------- | :---------
 [百度地图][baidu-map]       | `17359270396` | `17359270396`                    | `xmzc2578`
 [高德地图][amap]            | `17359270396` | `xiamenzhengchen@163.com`        | `xmzc2578`
 [微信开放平台][open-weixin] | `15011344742` | `fashionworld@fashionworld.live` | `Boruizhida2016`
 [微博开放平台][open-weibo]  |               | `boruizhida@126.com`             | `boruizhida2016`
+[七牛云][qiniu]             |               | `xiamenzhengchen@163.com`        | `XMZC2017`
 
 [baidu-map]: http://lbsyun.baidu.com/
 [amap]: https://lbs.amap.com/
 [open-weixin]: https://open.weixin.qq.com/
 [open-weibo]: http://open.weibo.com/
+[qiniu]: https://www.qiniu.com/
