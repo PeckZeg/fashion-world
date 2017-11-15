b41d40215d80ccec6afc0f0ab4f97edf49d0dfcf
PeckZeg <peck.zeg@gmail.com>
2017-11-15 10:00:05 +0800

    update handler

--- api/client/video/post/qiniu/handler.js
+++ api/client/video/post/qiniu/handler.js
@@ -1,13 +1,58 @@
+const path = require('path');
+
+const createBucketManager = require('utils/qiniu/createBucketManager');
+const fetchFileSha1 = require('utils/qiniu/fetchFileSha1');
 const handleError = require('utils/response/handleError');
+const fetchVideoInfo = require('utils/qiniu/fetchVideoInfo');
+
+const Video = require('models/Video');
+
+const { videos: bucket } = config.qiniu.bucket;
 
 module.exports = async (req, res, next) => {
   try {
     const { inputKey: source, items } = req.body;
+    const bucketManager = createBucketManager();
     let { key } = items[0];
 
-    console.log({source,key});
+    let video = Video.findOne({ source });
 
-    res.send({source,key})
+    if (video) {
+      const { mimeType } = await fetchStat(bucketManager, bucket, key);
+      const sha1 = await fetchFileSha1('videos', key);
+      const extname = path.extname(key);
+      const definition = `${sha1}${extname}`;
+      const [respBody, respInfo] = await bucketManager.moveAsync(bucket, key,
+          bucket, definition, { force: true });
+
+      if (respInfo.statusCode !== 200) {
+        throw new ResponseError(respInfo.statusCode, respBody);
+      }
+
+      const avinfo = await fetchVideoInfo('videos', definition);
+      const { height } = avinfo.streams[0];
+      const doc = {
+        $push: {
+          definitions: { type: `${height}p`, source: definition }
+        }
+      };
+      const opts = { new: true };
+
+      video = await Video.findByIdAndUpdate(video._id, doc, opts);
+      video = video.toJSON();
+
+      res.send({ video });
+    }
+
+    else {
+      const [respBody, respInfo] = await bucketManager.deleteAsync(bucket, key);
+
+      if (respInfo.statusCode !== 200) {
+        throw new ResponseError(respInfo.statusCode, respBody);
+      }
+    }
+
+    res.send({ video });
   }
 
   catch (err) {
---
+++ utils/global-mixins/promisify/redis.js
@@ -0,0 +1,5 @@
+const bluebird = require('bluebird');
+const redis = require('redis');
+
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
