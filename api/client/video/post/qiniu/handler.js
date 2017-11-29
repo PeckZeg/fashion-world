3e42420d4a5280ccb7bbde165c5ad6e2753a7ad6
PeckZeg <peck.zeg@gmail.com>
2017-11-29 10:23:34 +0800

    debug

--- api/client/video/post/qiniu/handler.js
+++ api/client/video/post/qiniu/handler.js
@@ -10,49 +10,15 @@ const handleError = require('utils/response/handleError');
 const SourceVideo = require('models/SourceVideo');
 const Video = require('models/Video');
 
+const { successKey, errorKey } = require('scripts/migrateVideo/keys/qiniu');
 const { videos: bucket } = config.qiniu.bucket;
 
 module.exports = async (req, res, next) => {
-  if (isEmpty(req.body)) {
-    return res.send({ video: null });
+  if (!isEmpty(req.body)) {
+
   }
 
-  try {
-    const { inputKey: source, items } = req.body;
-    const bucketManager = createBucketManager();
-    let video = await Video.findOne({ source });
+  console.log(JSON.stringify(req.body));
 
-    if (video) {
-      const definitions = await Promise.all(items.map(async ({ key }) => {
-        const sha1 = await fetchFileSha1('videos', key);
-        const extname = path.extname(key);
-        const definition = `${sha1}${extname}`;
-        const [respBody, respInfo] = await bucketManager.moveAsync(bucket, key,
-          bucket, definition, { force: true });
-
-        if (respInfo.statusCode !== 200) {
-          throw new ResponseError(respInfo.statusCode, respBody);
-        }
-
-        const avinfo = await fetchVideoInfo('videos', definition);
-        const { height } = avinfo.streams[0];
-
-        return { type: `${height}p`, source: definition };
-      }));
-
-      const doc = { $set: { definitions } };
-      const opts = { new: true };
-
-      video = await Video.findByIdAndUpdate(video._id, doc, opts);
-      video = video.toJSON();
-    }
-
-    console.log({video});
-
-    res.send({ video });
-  }
-
-  catch (err) {
-    handleError(res, err);
-  }
+  res.send({ message: 'ok' });
 };
---
+++ scripts/migrateVideo/keys/qiniu.js
@@ -0,0 +1,5 @@
+// 七牛转换队列成功哈希表
+exports.successKey = 'script:migrate:qiniu-success-hash';
+
+// 转换队列失败哈希表
+exports.errorKey = 'script:migrate:qiniu-error-hash';
