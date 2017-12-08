ce1f9dd8ec620a784e6a1af9746c13b3707bbeac
PeckZeg <peck.zeg@gmail.com>
2017-12-08 15:24:34 +0800

    bug ix

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const restc = require('restc');
 const glob = require('glob');
 const path = require('path');
 const url = require('url');
-const appendAccessControlAllowOrigin = reqlib('./utils/router/appendAccessControlAllowOrigin');
 
 const globalMixins = require('./utils/global-mixins');
 const app = express();
@@ -36,7 +35,7 @@ app.use(cookieParser());
 app.use('/static', express.static('/data/static'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/static', express.static(path.join(__dirname, '/admin-templates/static')));
-app.use('/api', appendAccessControlAllowOrigin);
+app.use('/api', reqlib('./utils/router/appendAccessControlAllowOrigin'));
 
 // redirect for `cms.fashionworldcn.com`
 app.get('/', (req, res, next) => {
