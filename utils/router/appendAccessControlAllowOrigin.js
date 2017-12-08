50ae81b3e1a2e2f77bb0c252c8d1c796fee7e438
PeckZeg <peck.zeg@gmail.com>
2017-12-08 15:27:24 +0800

    b

--- utils/router/appendAccessControlAllowOrigin.js
+++ utils/router/appendAccessControlAllowOrigin.js
@@ -3,7 +3,7 @@ const url = require('url');
 module.exports = (req, res, next) => {
   const { protocol, hostname } = req;
 
-  console.log(hostname);
+  console.log(req)
 
   if (config.accessControlAllowOrigin.indexOf(hostname) > -1) {
     res.append('Access-Control-Allow-Origin', url.format({ protocol, hostname }));
